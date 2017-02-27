c818f456f476ddb6291f10761efb8a50defe0d52
Beth Rice <er113@imperial.ac.ic.uk>
2017-02-27 16:02:50 +0000

    Finds unit cell after transformations and translations

--- Box Sync/PhD/CrystalStructures/Transform_molecules.py
+++ Box Sync/PhD/CrystalStructures/Transform_molecules.py
@@ -7,36 +7,38 @@ import sys
 import Spacegroup
 
 
-def read_in_files():
-	data = sys.argv[1]
-
-	# Extract raw data from xyz file (i.e. discard atom symbols)
-	labels = np.genfromtxt(data,usecols=0,dtype=str)
-	coordfile = np.genfromtxt(data)[:,1:]
-
-	# print "Coordinates: ", coordfile
-	n=int(sys.argv[2])
-
-	# Cell lengths
-	x=float(sys.argv[3])
-	y=float(sys.argv[4])
-	z=float(sys.argv[5])
-
-	# Read in space group: Pbca, Cc, P21c, P21, Pna21, Pbcn, P212121, C2c, P_1, 		Pca21, C2
-	SG = sys.argv[6]
-
-	# File name for output files
-	filename = sys.argv[7]
+# --------------------------------- Read in data --------------------------------------- #
 
 
+data = sys.argv[1]
 
-def transform_and_translate():
+# Extract raw data from xyz file (i.e. discard atom symbols)
+LABELS = np.genfromtxt(data,usecols=0,dtype=str)
+COORDFILE = np.genfromtxt(data)[:,1:]
 
-	# Cell matrix
-	cell= [  [x,           0.0000000000,         0.0000000000,],
-    	   [0.0000000000,        y,              0.0000000000,],
-    	   [0.0000000000,        0.0000000000,         z] ]
+# print "Coordinates: ", coordfile
+N=int(sys.argv[2])
 
+# Cell lengths
+x=float(sys.argv[3])
+y=float(sys.argv[4])
+z=float(sys.argv[5])
+
+# Read in space group: Pbca, Cc, P21c, P21, Pna21, Pbcn, P212121, C2c, P_1, 		Pca21, C2
+SG = sys.argv[6]
+
+# File name for output files
+FILENAME = sys.argv[7]
+
+# Cell matrix
+CELL= [  [x,           0.0000000000,         0.0000000000,],
+[0.0000000000,        y,              0.0000000000,],
+[0.0000000000,        0.0000000000,         z] ]
+
+
+# -------------------------------------------------------------------------------------- #
+
+def transform_and_translate(coordfile):
 
 	# Transform to fractional coordinates
 	coordfile=np.inner(np.linalg.inv(cell),coordfile).T
@@ -83,6 +85,8 @@ def transform_and_translate():
     	          [[1,0,0,-1],[0,1,0,-1],[0,0,1,-1],[0,0,0,1]]
     	          ]
 
+	all_transformations = ((1,N,3))
+
 	for displace_n,displace in enumerate(Translations):
 	    for transform_n,transform in enumerate(Transforms):
 	        # Transform coordinates
@@ -93,22 +97,35 @@ def transform_and_translate():
 	        trans=trans[:,[0,1,2]]
 	        # Scale up to real coordinates
 	        trans=np.inner(trans,cell)
+			all_transformations=np.vstack(all_transformations,trans[None,...])
 
-	return trans
+	print all_transformations			
 
-
-
-
-
+	return all_transformations
 
 
+def check_in_cell(coordfile):
+	coordfile_fractional=np.inner(np.linalg.inv(cell),coordfile).T
 
-def save_files(file_to_save,labels,filename):
-		
+	for i in range(0,N):
+    if 0<coordfile[i,0]<1 and 0<coordfile[i,1]<1 and 0<coordfile[i,2]<1:
+        atoms=np.append(atoms,i)
+
+	return len(atoms)
+
+
+def unit_cell():
+	
+
+
+
+
+
+def save_files(file_to_save,labels,FILENAME,i):
 	# Put labels back to save in xyz format
 	trans=np.array(zip(labels,file_to_save[:,0],file_to_save[:,1],file_to_save[:,2]),dtype=		[('labels','S8'),('trans[:,0]',float),('trans[:,1]',float),('trans[:,2]',float)])
 	# Save
-	np.savetxt("%s_%d.xyz"%(filename,transform_n+displace_n*m),trans,delimiter=" 	",fmt=["%s"]+["%f"]+["%f"]+["%f"])
+	np.savetxt("%s_%d.xyz"%(FILENAME,i),trans,delimiter=" 	",fmt=["%s"]+["%f"]+["%f"]+["%f"])
 
 
 
