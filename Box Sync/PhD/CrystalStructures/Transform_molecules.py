2c7701729e73c13e2d58fbb03393f5520fcad685
Beth Rice <bethrice44@gmail.com>
2017-03-05 14:24:19 +0000

    Saves separate files of all molecules in unit cell

--- Box Sync/PhD/CrystalStructures/Transform_molecules.py
+++ Box Sync/PhD/CrystalStructures/Transform_molecules.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# Transform molcules to equivalent positions and then translate, then make xyz files of each in supercell for calculation of J
+# Transform molcules to equivalent positions and then translate, then find molecules in the unit cell to make xyz files of each in supercell for calculation of J
+
 
 import numpy as np
 import sys
@@ -9,7 +10,6 @@ import Spacegroup
 
 # --------------------------------- Read in data --------------------------------------- #
 
-
 data = sys.argv[1]
 
 # Extract raw data from xyz file (i.e. discard atom symbols)
@@ -17,24 +17,61 @@ LABELS = np.genfromtxt(data,usecols=0,dtype=str)
 COORDFILE = np.genfromtxt(data)[:,1:]
 
 # print "Coordinates: ", coordfile
-N=int(sys.argv[2])
+N=len(COORDFILE)
 
 # Cell lengths
-x=float(sys.argv[3])
-y=float(sys.argv[4])
-z=float(sys.argv[5])
+x=float(sys.argv[2])
+y=float(sys.argv[3])
+z=float(sys.argv[4])
  
 # Read in space group: Pbca, Cc, P21c, P21, Pna21, Pbcn, P212121, C2c, P_1,Pca21, C2
-SG = sys.argv[6]
+SG = sys.argv[5]
 
 # File name for output files
-FILENAME = sys.argv[7]
+filename = sys.argv[6]
 
 # Cell matrix
 CELL= [  [x,           0.0000000000,         0.0000000000,],
 [0.0000000000,        y,              0.0000000000,],
 [0.0000000000,        0.0000000000,         z] ]
 
+
+# -------------------------------------------------------------------------------------- #
+
+def translations():
+
+# Translation matrices
+
+	Translations=[ [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],
+              [[1,0,0,1],[0,1,0,0],[0,0,1,0],[0,0,0,1]],
+              [[1,0,0,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]],
+              [[1,0,0,0],[0,1,0,0],[0,0,1,1],[0,0,0,1]],
+              [[1,0,0,-1],[0,1,0,0],[0,0,1,0],[0,0,0,1]],
+              [[1,0,0,0],[0,1,0,-1],[0,0,1,0],[0,0,0,1]],
+              [[1,0,0,0],[0,1,0,0],[0,0,1,-1],[0,0,0,1]],
+              [[1,0,0,1],[0,1,0,1],[0,0,1,0],[0,0,0,1]],
+              [[1,0,0,1],[0,1,0,-1],[0,0,1,0],[0,0,0,1]],
+              [[1,0,0,-1],[0,1,0,1],[0,0,1,0],[0,0,0,1]],
+              [[1,0,0,-1],[0,1,0,-1],[0,0,1,0],[0,0,0,1]],
+              [[1,0,0,1],[0,1,0,0],[0,0,1,1],[0,0,0,1]],
+              [[1,0,0,1],[0,1,0,0],[0,0,1,-1],[0,0,0,1]],
+              [[1,0,0,-1],[0,1,0,0],[0,0,1,1],[0,0,0,1]],
+              [[1,0,0,-1],[0,1,0,0],[0,0,1,-1],[0,0,0,1]],
+              [[1,0,0,0],[0,1,0,1],[0,0,1,1],[0,0,0,1]],
+              [[1,0,0,0],[0,1,0,1],[0,0,1,-1],[0,0,0,1]],
+              [[1,0,0,0],[0,1,0,-1],[0,0,1,1],[0,0,0,1]],
+              [[1,0,0,0],[0,1,0,-1],[0,0,1,-1],[0,0,0,1]],
+              [[1,0,0,1],[0,1,0,-1],[0,0,1,-1],[0,0,0,1]],
+              [[1,0,0,-1],[0,1,0,-1],[0,0,1,1],[0,0,0,1]],
+              [[1,0,0,-1],[0,1,0,1],[0,0,1,-1],[0,0,0,1]],
+              [[1,0,0,-1],[0,1,0,1],[0,0,1,1],[0,0,0,1]],
+              [[1,0,0,1],[0,1,0,-1],[0,0,1,1],[0,0,0,1]],
+              [[1,0,0,1],[0,1,0,1],[0,0,1,-1],[0,0,0,1]],
+              [[1,0,0,1],[0,1,0,1],[0,0,1,1],[0,0,0,1]],
+              [[1,0,0,-1],[0,1,0,-1],[0,0,1,-1],[0,0,0,1]]
+              ]
+
+	return Translations
 
 # -------------------------------------------------------------------------------------- #
 
@@ -52,42 +89,13 @@ def transform_and_translate(coordfile,cell):
 
 	Transforms = np.array(Spacegroup.choose(SG))
 
-	print Transforms 
-
-	# Translation matrices
-
-	Translations=[ [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]],
-    	          [[1,0,0,1],[0,1,0,0],[0,0,1,0],[0,0,0,1]],
-    	          [[1,0,0,0],[0,1,0,1],[0,0,1,0],[0,0,0,1]],
-    	          [[1,0,0,0],[0,1,0,0],[0,0,1,1],[0,0,0,1]],
-    	          [[1,0,0,-1],[0,1,0,0],[0,0,1,0],[0,0,0,1]],
-    	          [[1,0,0,0],[0,1,0,-1],[0,0,1,0],[0,0,0,1]],
-    	          [[1,0,0,0],[0,1,0,0],[0,0,1,-1],[0,0,0,1]],
-    	          [[1,0,0,1],[0,1,0,1],[0,0,1,0],[0,0,0,1]],
-    	          [[1,0,0,1],[0,1,0,-1],[0,0,1,0],[0,0,0,1]],
-    	          [[1,0,0,-1],[0,1,0,1],[0,0,1,0],[0,0,0,1]],
-    	          [[1,0,0,-1],[0,1,0,-1],[0,0,1,0],[0,0,0,1]],
-    	          [[1,0,0,1],[0,1,0,0],[0,0,1,1],[0,0,0,1]],
-    	          [[1,0,0,1],[0,1,0,0],[0,0,1,-1],[0,0,0,1]],
-    	          [[1,0,0,-1],[0,1,0,0],[0,0,1,1],[0,0,0,1]],
-    	          [[1,0,0,-1],[0,1,0,0],[0,0,1,-1],[0,0,0,1]],
-    	          [[1,0,0,0],[0,1,0,1],[0,0,1,1],[0,0,0,1]],
-    	          [[1,0,0,0],[0,1,0,1],[0,0,1,-1],[0,0,0,1]],
-    	          [[1,0,0,0],[0,1,0,-1],[0,0,1,1],[0,0,0,1]],
-    	          [[1,0,0,0],[0,1,0,-1],[0,0,1,-1],[0,0,0,1]],
-    	          [[1,0,0,1],[0,1,0,-1],[0,0,1,-1],[0,0,0,1]],
-    	          [[1,0,0,-1],[0,1,0,-1],[0,0,1,1],[0,0,0,1]],
-    	          [[1,0,0,-1],[0,1,0,1],[0,0,1,-1],[0,0,0,1]],
-    	          [[1,0,0,-1],[0,1,0,1],[0,0,1,1],[0,0,0,1]],
-    	          [[1,0,0,1],[0,1,0,-1],[0,0,1,1],[0,0,0,1]],
-    	          [[1,0,0,1],[0,1,0,1],[0,0,1,-1],[0,0,0,1]],
-    	          [[1,0,0,1],[0,1,0,1],[0,0,1,1],[0,0,0,1]],
-    	          [[1,0,0,-1],[0,1,0,-1],[0,0,1,-1],[0,0,0,1]]
-    	          ]
+    #print Transforms,np.shape(Transforms)
+    
+	m=np.shape(Transforms)[0]
 
 	all_transformations = np.zeros((1,N,3))
 
-	for displace_n,displace in enumerate(Translations):
+	for displace_n,displace in enumerate(translations()):
 	    for transform_n,transform in enumerate(Transforms):
 	        # Transform coordinates
 			trans=np.inner(transform,coordfile).T
@@ -98,11 +106,10 @@ def transform_and_translate(coordfile,cell):
 	        # Scale up to real coordinates
 			trans=np.inner(trans,cell)
 			all_transformations=np.vstack((all_transformations,trans[None,...]))
+    
+	return all_transformations[1:],m
 
-	print np.shape(all_transformations[1:])			
-
-	return all_transformations[1:]
-
+# -------------------------------------------------------------------------------------- #
 
 def check_in_cell(coordfile):
 	coordfile_fractional=np.inner(np.linalg.inv(CELL),coordfile).T
@@ -113,6 +120,7 @@ def check_in_cell(coordfile):
 
 	return len(atoms)
 
+# -------------------------------------------------------------------------------------- #
 
 def unit_cell(all_molecules):
 	in_cell=[0,0]
@@ -125,31 +133,56 @@ def unit_cell(all_molecules):
 	sorted_idx=np.argsort(in_cell[:,1])
 	in_cell_sorted=in_cell[sorted_idx][::-1]
 
-	unitcell=[0,0]
-	
+	unitcell_idx=[0,0]
+        
 	for i in range(len(in_cell_sorted)):
-		for j in range(len(all_molecules)/27):
-			if in_cell_sorted[i,1]%j not in unitcell[:,0]:
-				unitcell=np.vstack((unitcell,[in_cell_sorted[i,1]%j,in_cell_sorted[i,1]]))
+		for j in range(1,M):
+			if in_cell_sorted[i,0]%M==j and (j in unitcell_idx) == False:
+				unitcell_idx=np.vstack((unitcell_idx,[j,in_cell_sorted[i,0]]))
 
+
+	print unitcell_idx
+
+	unitcell=all_molecules[unitcell_idx[:,1],:,:]
 	print unitcell
-
 
 	return unitcell
 
+# -------------------------------------------------------------------------------------- #
 
-	
+def make_supercell(UNIT_CELL):
 
+    all_molecules = np.zeros((1,N,3))
 
+    for displace_n,displace in enumerate(translations()):
+    	for i in range(0,M):
+            trans=np.inner(np.linalg.inv(CELL),UNIT_CELL[i,:,:]).T
+            trans=np.c_[trans,np.ones(N)]
+            trans=np.inner(displace,trans).T
+            trans=trans[:,[0,1,2]]
+            trans=np.inner(trans,CELL)
+            all_molecules=np.vstack((all_molecules,trans[None,...]))
 
+    return all_molecules
+
+# -------------------------------------------------------------------------------------- #
 
 def save_files(file_to_save,labels,FILENAME,i):
 	# Put labels back to save in xyz format
-	trans=np.array(zip(labels,file_to_save[:,0],file_to_save[:,1],file_to_save[:,2]),dtype=		[('labels','S8'),('trans[:,0]',float),('trans[:,1]',float),('trans[:,2]',float)])
+	trans=np.array(zip(labels,file_to_save[:,0],file_to_save[:,1],file_to_save[:,2]),dtype=[('labels','S8'),('trans[:,0]',float),('trans[:,1]',float),('trans[:,2]',float)])
 	# Save
 	np.savetxt("%s_%d.xyz"%(FILENAME,i),trans,delimiter=" 	",fmt=["%s"]+["%f"]+["%f"]+["%f"])
 
-all_mols = transform_and_translate(COORDFILE,CELL)
-unit_cell(all_mols)
+
+# -------------------------------------------------------------------------------------- #
+
+
+all_trans,M = transform_and_translate(COORDFILE,CELL)
+unitcell=unit_cell(all_trans)
+all_mols=make_supercell(unitcell)
+
+for i in range(0,27*M):
+    save_files(all_mols[i,:,:],LABELS,filename,i)
+
 
 
